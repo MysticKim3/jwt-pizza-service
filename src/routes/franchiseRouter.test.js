91ea3fcd1746336be11403972b0637199be79a98
MysticKim3 <83422570+MysticKim3@users.noreply.github.com>
2024-09-08 16:18:26 -0600

    Add empty tests for create store, delete store, and delete franchise

---
+++ src/routes/franchiseRouter.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const app = require('../service');
+const { DB, Role } = require('../database/database.js');
+
+beforeAll(async () => {
+    //create admin user
+    //create a franchise
+})
+
+test('create store', async () => {
+
+})
+
+test('delete store', async () => {
+
+})
+
+test('delete franchise', async () => {
+    
+})
