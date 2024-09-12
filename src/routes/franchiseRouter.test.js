02aaf7dd073acb9a9c8ac0f7c98d48aeb214395d
MysticKim3 <83422570+MysticKim3@users.noreply.github.com>
2024-09-11 21:58:58 -0600

    Add base user and franchise to beforeAll

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -2,9 +2,32 @@ const request = require('supertest');
 const app = require('../service');
 const { DB, Role } = require('../database/database.js');
 
+async function createAdminUser() {
+    let user = { password: 'toofewsecrets', roles: [{ role: Role.Admin }] };
+    user.name = randomName();
+    user.email = user.name + '@admin.com';
+  
+    await DB.addUser(user);
+    return user;
+  }
+  
+  function randomName() {
+      return Math.random().toString(36).substring(2, 12);
+  }
+
+let adminUser;
+let adminAuthToken;
+let franchiseID;
+
 beforeAll(async () => {
-    //create admin user
-    //create a franchise
+    adminUser = await createAdminUser();
+    const req = {name: adminUser.name, email: adminUser.email, password: "toofewsecrets"};
+    const res = await request(app).put('/api/auth').send(req);
+    expect(res.status).toBe(200);
+    adminAuthToken = res.body.token;
+    const req2 = {name: "pizzaJoint", "admins": [{email: adminUser.email}]};
+    const franchise = await request(app).post('/api/franchise').set('Authorization', `Bearer ${adminAuthToken}`).send(req2);
+    franchiseID = franchise.body.id;
 })
 
 test('create store', async () => {
@@ -16,5 +39,13 @@ test('delete store', async () => {
 })
 
 test('delete franchise', async () => {
+
+})
+
+test('list franchises', async () => {
+
+})
+
+test('list user franchises', async () => {
     
 })
