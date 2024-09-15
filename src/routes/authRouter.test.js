0cc2ae3d63f72f5492c93a923ce3cf158fc097bf
MysticKim3 <83422570+MysticKim3@users.noreply.github.com>
2024-09-15 12:07:06 -0600

    Change linting error in authRouter test

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -32,6 +32,7 @@ test('login', async () => {
 
   const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
   expect(loginRes.body.user).toMatchObject(user);
+  expect(password).toBe('a');
 });
 
 test('update', async () => {
